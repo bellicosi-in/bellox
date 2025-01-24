3965a771204be124894d7a8e855bc3bc33b051bc
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2025-01-24 22:15:37 +0530

    global variables

--- vm.c
+++ vm.c
@@ -31,13 +31,15 @@ static void runtimeError(const char* format, ...){
 void initVM(){
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.globals);
   initTable(&vm.strings);
 
 }
 
 void freeVM(){
-  freeObjects();
+  freeTable(&vm.globals);
   freeTable(&vm.strings);
+  freeObjects();
 
 }
 
@@ -75,6 +77,7 @@ static void concatenate(){
 InterpretResult run(){
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() (AS_STRING(READ_CONSTANT()))
 #define BINARY_OP(valueType, op) do{ \
                                   if(!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))){ \
                                     runtimeError("operands must be numbers"); \
@@ -111,6 +114,33 @@ InterpretResult run(){
       case OP_NIL: push(NIL_VAL); break;
       case OP_TRUE: push(BOOL_VAL(true)); break;
       case OP_FALSE: push(BOOL_VAL(false)); break;
+      case OP_POP: pop(); break;
+      case OP_GET_GLOBAL: {
+        ObjString* name = READ_STRING();
+        Value value;
+        if(!tableGet(&vm.globals, name, &value)){
+          runtimeError("undefined variable %s", name->chars);
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(value);
+        break;
+      }
+      case OP_DEFINE_GLOBAL:{
+        ObjString* name = READ_STRING();
+        tableSet(&vm.globals, name, peek(0));
+        pop();
+        break;
+
+      }
+      case OP_SET_GLOBAL:{
+        ObjString* name = READ_STRING();
+        if(tableSet(&vm.globals, name, peek(0))){
+          tableDelete(&vm.globals, name);
+          runtimeError("undefined variable %s", name->chars);
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break;
+      }
       case OP_EQUAL: {
         Value b = pop();
         Value a = pop();
@@ -144,14 +174,19 @@ InterpretResult run(){
       case OP_MULTIPLY: BINARY_OP(NUMBER_VAL,*); break;
       case OP_DIVIDE: BINARY_OP(NUMBER_VAL,/); break;
       case OP_NOT: push(BOOL_VAL(isFalsey(pop()))); break;
-      case OP_RETURN:
+      case OP_PRINT: 
         printValue(pop());
         printf("\n");
+        break;
+      case OP_RETURN:
+        // printValue(pop());
+        // printf("\n");
         return INTERPRET_OK;
     }
   }
   
 #undef BINARY_OP
+#undef READ_STRING
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
